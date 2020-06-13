07a89cc5cb889ebed48d0b97dc00f1584942d64a
Emre <emre@edakik.com>
2020-06-13 16:02:57 +0300

    gelen kullaniciya gore task olusturma yapildi

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -3,17 +3,24 @@ import { Task } from "./task.entity";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { TaskStatus } from "./task-status.enum";
 import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
+import { User } from "src/auth/user.entity";
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task>{
-    async creteTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    async creteTask(
+        createTaskDto: CreateTaskDto,
+        user: User
+    ): Promise<Task> {
         const { title, description } = createTaskDto;
 
         const task = new Task();
         task.title = title;
         task.description = description;
         task.status = TaskStatus.OPEN;
+        task.user = user;
         await task.save();
+        
+        delete task.user;
 
         return task;
     }
